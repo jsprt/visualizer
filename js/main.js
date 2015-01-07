6c15c66cf93653564578a5a19a4506aae21d3178
jasoffice <jaspreet.channa@gmail.com>
2015-01-07 17:05:17 +1100

    test

--- js/main.js
+++ js/main.js
@@ -2,4 +2,4 @@ var height = 600, width = 900;
 var container = d3.select("body")
                             .append("svg")
                             .attr("height",height)
-                            .height("width",width);
+                            .attr("width",width);
