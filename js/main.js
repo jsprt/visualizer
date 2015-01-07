db1f553bcd937eea0defb969e1151fff825d5a10
jasoffice <jaspreet.channa@gmail.com>
2015-01-07 17:34:29 +1100

    test

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
+
 var height = 600, width = 900;
 var container = d3.select("body")
                             .append("svg")
                             .attr("height",height)
                             .attr("width",width);
+d3.json("json/miserables.json", function(json){
+data = json;
+   console.log(data.links.length);
+} )
+
+var circle = svg.selectAll("circle")
