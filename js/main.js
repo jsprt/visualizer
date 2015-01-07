4fc72ff55837a746b1d53c68e18653323c325991
jasoffice <jaspreet.channa@gmail.com>
2015-01-07 15:59:56 +1100

    first commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var height = 600, width = 900;
+var container = d3.select("body")
+                            .append("svg")
+                            .attr("height",height)
+                            .height("width",width);
