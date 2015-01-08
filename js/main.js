89605ccc7f689fb14bf03a6a1e4ac991e9230bc9
jasoffice <jaspreet.channa@gmail.com>
2015-01-08 17:19:55 +1100

    work

--- js/main.js
+++ js/main.js
@@ -18,36 +18,68 @@ var palette = {
     "green": "#259286",
     "yellowgreen": "#738A05"
 }
-var height = 600, width = 900, devices, force;
+var height = 6000, width = 3000, devices,connection, force, curr_location = 0, loc_node_count =0;
 var container = d3.select("body")
                             .append("svg")
                             .attr("height",height)
                             .attr("width",width);
+
+container.attr("opacity", 1e-6)
+    .transition()
+    .duration(2000)
+    .attr("opacity", 1);
+
 d3.json("json/miserables.json", function(json){
 data = json;
-
-    init();
-    force.start();
-
+    init(data);
 } );
 
 
 
-function init(){
-    devices = container.selectAll('rect')
+function init(data){
+
+
+
+/*    devices = container.selectAll('rect')
         .data(data.nodes)
         .enter()
         .append('rect')
         .attr("width", 50)
-        .attr("height",50)
+        .attr("height",50)*/
+
+/*     connection = container.selectAll(".link")
+         .data(data.links)
+         .enter().append("line")
+         .attr("class", "link")
+         .style("stroke-width", function(d) { return Math.sqrt(d.value); });*/
 
     force = d3.layout.force()
-        .nodes(devices)
-        .links([])
-        .gravity(0.3)
-        .charge(-1000)
+        .nodes(data.nodes)
+        .links(data.links)
         .size([width,height]);
 
+
+/*    force.on("tick" ,function(){
+        devices.attr("x", function(d){
+            if(curr_location != d.location){
+                curr_location = d.location;
+                loc_node_count =1;
+            }
+            else {
+                loc_node_count++;
+            }
+            return 400 * d.location + (loc_node_count)*100 ;
+        });
+
+        devices.attr("y", function(d){
+            return 400;
+        });
+
+    });*/
+
+
+    force.start();
 }
+
 
 
